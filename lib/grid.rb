85836172e2866cae94dec3b8a0188ac06f3e838a
endulum <71853916+endulum@users.noreply.github.com>
2025-07-14 09:42:11 -0700

    draft tests for `Grid` printing

--- lib/grid.rb
+++ lib/grid.rb
@@ -37,4 +37,8 @@ class Grid
       end
     end
   end
+
+  def print
+    "owo"
+  end
 end
--- spec/grid.spec.rb
+++ spec/grid.spec.rb
@@ -106,4 +106,59 @@ describe Grid do
       end
     end
   end
+
+  describe "printing the grid" do
+    context "when grid is empty" do
+      it "prints an empty grid" do
+        text = ""
+        File.open("spec/sample_grids/empty.txt") { |file| text = file.gets }
+        expect(grid.print).to be text
+      end
+    end
+
+    context "when grid has one red token and one yellow token" do
+      subject(:grid_with_tokens) do
+        grid = described_class.new
+        grid.drop_token(:red, 0)
+        grid.drop_token(:yellow, grid.column_count - 1)
+        return grid
+      end
+
+      it "prints a grid with one red token and one yellow token" do
+        text = ""
+        File.open("spec/sample_grids/one-red-one-yellow.txt") { |file| text = file.gets }
+        expect(grid_with_tokens.print).to be text
+      end
+    end
+
+    context "when grid has full red column and full yellow column" do
+      subject(:grid_with_columns) do
+        grid = described_class.new
+        6.times { grid.drop_token(:red, 0) }
+        6.times { grid.drop_token(:yellow, grid.column_count - 1) }
+        return grid
+      end
+
+      it "prints a grid with full red column and full yellow column" do
+        text = ""
+        File.open("spec/sample_grids/col-red-col-yellow.txt") { |file| text = file.gets }
+        expect(grid_with_columns.print).to be text
+      end
+    end
+
+    context "when grid has full red row and full yellow row" do
+      subject(:grid_with_rows) do
+        grid = described_class.new
+        grid.column_count.times { |column| grid.drop_token(:red, column) }
+        grid.column_count.times { |column| grid.drop_token(:yellow, column) }
+        return grid
+      end
+
+      it "prints a grid with full red row and full yellow row" do
+        text = ""
+        File.open("spec/sample_grids/row-red-row-yellow.txt") { |file| text = file.gets }
+        expect(grid_with_rows.print).to be text
+      end
+    end
+  end
 end
