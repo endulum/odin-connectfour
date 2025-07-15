020ad2a360cb03c8f4171c75b06690c69c5033e2
endulum <71853916+endulum@users.noreply.github.com>
2025-07-15 14:52:05 -0700

    fix typos

--- spec/player_loop.spec.rb
+++ spec/player_loop.spec.rb
@@ -4,7 +4,7 @@ require_relative "../lib/grid"
 describe PlayerLoop do
   describe "prompting input" do
     context "when input is valid, and column is not full" do
-      subject(:player_loop) { described_class.new(:red, bob, Grid.new) }
+      subject(:player_loop) { described_class.new("bob", :red, Grid.new) }
 
       before do
         valid_input = "0"
@@ -18,8 +18,8 @@ describe PlayerLoop do
     end
 
     context "when input is valid, and column is full" do
-      let!(grid) { Grid.new }
-      subject(:player_loop) { described_class.new(:red, bob, grid) }
+      let!(:grid) { Grid.new }
+      subject(:player_loop) { described_class.new("bob", :red, grid) }
 
       before do
         6.times { grid.drop_token(player_loop.color, 0) }
@@ -37,7 +37,7 @@ describe PlayerLoop do
     end
 
     context "when input is invalid" do
-      subject(:player_loop) { described_class.new(:red, bob, Grid.new) }
+      subject(:player_loop) { described_class.new("bob", :red, Grid.new) }
 
       before do
         invalid_input = "owo"
