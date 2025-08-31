112d065d4ba8f3143013911c16741bf8aaa397d3
endulum <71853916+endulum@users.noreply.github.com>
2025-08-30 18:56:38 -0700

    complete `Grid` behavior
    - remake the base `Grid` class
    - use a module `Matchable` to contain n-in-a-row determination
    - remake the `Grid` spec to test for both `Grid` and `Matchable` behavior

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,2 +1,65 @@
+require_relative "matchable"
+
+# a two-dimensional grid accessible and manipulable by x,y coordinates
 class Grid
+  include Matchable
+
+  attr_reader :values, :column_count, :row_count
+
+  def initialize(columns, rows)
+    @column_count = columns
+    @row_count = rows
+    @values = Array.new(columns) { Array.new(rows) }
+  end
+
+  def at(column, row)
+    values.dig(column, row)
+  end
+
+  def set_value_at(column, row, value)
+    return unless column < @column_count && row < @row_count
+
+    values[column][row] = value
+  end
+
+  def each
+    @column_count.times do |column_index|
+      @row_count.times do |row_index|
+        yield at(column_index, row_index), [column_index, row_index]
+        # usage: this.each do |value, coords| ...
+      end
+    end
+  end
+
+  def map
+    mapped = []
+    each do |value|
+      block_given? ? mapped.push(yield value) : mapped.push(value)
+    end
+    mapped
+  end
+
+  def at_column(index)
+    values[index]
+  end
+
+  def each_column
+    @column_count.times do |column_index|
+      yield at_column(column_index), column_index
+      # usage: this.each_column do |column, index| ...
+    end
+  end
+
+  def at_row(index)
+    row = []
+    each_column { |column| row.push column[index] }
+    row
+  end
+
+  def each_row
+    @row_count.times do |row_index|
+      yield at_row(row_index), row_index
+      # usage: this.each_row do |row, index| ...
+    end
+  end
 end
--- spec/grid.spec.rb
+++ spec/grid.spec.rb
@@ -1,4 +1,153 @@
 require_relative "../lib/grid"
 
 describe Grid do
+  subject(:grid) { described_class.new(4, 4) }
+
+  describe "#set_value_at" do
+    it "sets a value using x,y coordinates" do
+      grid.column_count.times do |col|
+        grid.row_count.times do |row|
+          grid.set_value_at(col, row, :color)
+        end
+      end
+
+      grid.column_count.times do |col|
+        grid.row_count.times do |row|
+          expect(grid.at(col, row)).to eq :color
+        end
+      end
+    end
+  end
+
+  describe "#each" do
+    before do
+      grid.column_count.times do |col|
+        grid.row_count.times do |row|
+          grid.set_value_at(col, row, :color)
+        end
+      end
+    end
+
+    it "iterates through each cell in the grid" do
+      values = grid.map
+      expect(values).to eq Array.new(16).fill(:color)
+    end
+  end
+
+  describe "axial enumerables" do
+    subject(:grid) do
+      grid = described_class.new(4, 4)
+      grid.column_count.times do |col|
+        # horizontally "striping" the grid with colors
+        color = if col % 3 == 0
+                  :blue
+                else
+                  col.even? ? :red : :yellow
+                end
+        grid.row_count.times do |row|
+          grid.set_value_at(col, row, color)
+        end
+      end
+      return grid
+    end
+
+    describe "#each_column" do
+      it "iterates through columns of the grid" do
+        columns = []
+        expected_columns = [
+          [:blue] * 4,
+          [:yellow] * 4,
+          [:red] * 4,
+          [:blue] * 4
+        ]
+
+        grid.each_column { |column| columns.push(column) }
+        expect(columns).to eq expected_columns
+      end
+    end
+
+    describe "#each_row" do
+      it "iterates through rows of the grid" do
+        rows = []
+        expected_rows = [%i[blue yellow red blue]] * 4
+
+        grid.each_row { |row| rows.push(row) }
+        expect(rows).to eq expected_rows
+      end
+    end
+  end
+
+  describe "#match_at?" do
+    describe "horizontal matching" do
+      subject(:grid) do
+        grid = described_class.new(4, 4)
+        # horizontal row of three
+        grid.set_value_at(0, 0, :red)
+        grid.set_value_at(1, 0, :red)
+        grid.set_value_at(2, 0, :red)
+        return grid
+      end
+
+      it "matches" do
+        expect { grid.set_value_at(3, 0, :red) }
+          .to change { grid.match_at?([0, 0], :red, 4) }
+          .from(false)
+          .to(true)
+      end
+    end
+
+    describe "vertical matching" do
+      subject(:grid) do
+        grid = described_class.new(4, 4)
+        # vertical row of three
+        grid.set_value_at(0, 0, :red)
+        grid.set_value_at(0, 1, :red)
+        grid.set_value_at(0, 2, :red)
+        return grid
+      end
+
+      it "matches" do
+        expect { grid.set_value_at(0, 3, :red) }
+          .to change { grid.match_at?([0, 0], :red, 4) }
+          .from(false)
+          .to(true)
+      end
+    end
+
+    describe "forward-diagonal matching" do
+      subject(:grid) do
+        grid = described_class.new(4, 4)
+        # forward diagonal row of three
+        grid.set_value_at(0, 0, :red)
+        grid.set_value_at(1, 1, :red)
+        grid.set_value_at(2, 2, :red)
+        return grid
+      end
+
+      it "matches" do
+        expect { grid.set_value_at(3, 3, :red) }
+          .to change { grid.match_at?([0, 0], :red, 4) }
+          .from(false)
+          .to(true)
+      end
+    end
+
+    describe "backward-diagonal matching" do
+      subject(:grid) do
+        grid = described_class.new(4, 4)
+        # backward diagonal row of three
+        grid.set_value_at(0, 3, :red)
+        grid.set_value_at(1, 2, :red)
+        grid.set_value_at(2, 1, :red)
+        return grid
+      end
+
+      it "matches" do
+        expect { grid.set_value_at(3, 0, :red) }
+          .to change { grid.match_at?([0, 3], :red, 4) }
+          .from(false)
+          .to(true)
+      end
+    end
+  end
 end
