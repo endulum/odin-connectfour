b72c0d08b87c7eb7e6d56578d97c9ac60cfda223
endulum <71853916+endulum@users.noreply.github.com>
2025-09-05 11:58:18 -0700

    create method and spec for beginning a game

--- lib/game/game_loop.rb
+++ lib/game/game_loop.rb
@@ -1,4 +1,5 @@
 require_relative "player_init"
+require_relative "../connect_four_board"
 
 require "colorize"
 
@@ -6,5 +7,10 @@ require "colorize"
 class GameLoop
   include PlayerInit
 
-  attr_reader :player_one, :player_two, :play_mode
+  attr_reader :player_one, :player_two, :play_mode, :board, :whose_turn
+
+  def begin_game
+    @board = ConnectFourBoard.new
+    @whose_turn = player_one
+  end
 end
--- spec/game_loop/player_turn.spec.rb
+++ spec/game_loop/player_turn.spec.rb
@@ -1,5 +1,6 @@
 require_relative "../../lib/game/game_loop"
 require_relative "../../lib/prompt"
+require_relative "../../lib/connect_four_board"
 require "colorize"
 
 describe GameLoop do
@@ -25,5 +26,19 @@ describe GameLoop do
       { name: "alice", color: :yellow }
     )
     return game
+  end
+
+  describe "#begin_game" do
+    before do
+      game_1p.begin_game
+    end
+
+    it "sets up the board" do
+      expect(game_1p.board).to be_instance_of ConnectFourBoard
+    end
+
+    it "ensures player 1 goes first" do
+      expect(game_1p.whose_turn).to eq game_1p.player_one
+    end
   end
 end
