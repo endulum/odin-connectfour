cf253c54b661e28e0e205bbdbf2e63c25961c180
endulum <71853916+endulum@users.noreply.github.com>
2025-09-07 13:52:18 -0700

    test and draft `PlayerTurn#prompt_player_move`

--- lib/game/player_turn.rb
+++ lib/game/player_turn.rb
@@ -25,6 +25,30 @@ module PlayerTurn
   end
 
   def prompt_player_move
-    #
+    text = build_prompt_player_text
+    loop do
+      column = Prompt.get_input text
+      return column if valid_column?(column)
+    end
+  end
+
+  private
+
+  def build_prompt_player_text
+    player_name = @whose_turn[:name]
+    colored_name = player_name.colorize(color: @whose_turn[:color])
+    player_token = @whose_turn[:color].to_s
+    colored_token = player_token.colorize(color: @whose_turn[:color])
+    "It's your turn, #{colored_name}. Place a #{colored_token} token in which column?\n> ".bold
+  end
+
+  def valid_column?(input)
+    error = if !%w[0 1 2 3 4 5 6].include? input
+              "please enter a column number 0 through 6!"
+            elsif @board.column_full? input.to_i
+              "that column is full, choose another!"
+            end
+    puts "Input error: #{error}".bold if error
+    error.nil?
   end
 end
--- spec/game_loop/player_turn.spec.rb
+++ spec/game_loop/player_turn.spec.rb
@@ -92,4 +92,95 @@ describe GameLoop do
       end
     end
   end
+
+  describe "#prompt_player_move" do
+    describe "printing the prompt" do
+      context "when 'bob' with a :red token" do
+        before do
+          allow(game).to receive(:valid_column?).and_return true
+          player_one = game.instance_variable_get(:@player_one)
+          game.instance_variable_set(:@whose_turn, player_one)
+        end
+
+        it "prints properly" do
+          text = "It's your turn, #{'bob'.red}. Place a #{'red'.red} token in which column?\n> ".bold
+          expect(Prompt).to receive(:print).with text
+          game.prompt_player_move
+        end
+      end
+
+      context "when 'alice' with a :yellow token" do
+        before do
+          allow(game).to receive(:valid_column?).and_return true
+          player_two = { name: "alice", color: :yellow }
+          game.instance_variable_set(:@player_two, player_two)
+          game.instance_variable_set(:@play_mode, "2p")
+          game.instance_variable_set(:@whose_turn, player_two)
+        end
+
+        it "prints properly" do
+          text = "It's your turn, #{'alice'.yellow}. Place a #{'yellow'.yellow} token in which column?\n> ".bold
+          expect(Prompt).to receive(:print).with text
+          game.prompt_player_move
+        end
+      end
+    end
+
+    describe "getting the input" do
+      subject(:game) do
+        game = described_class.new
+        player_one = { name: "bob", color: :red }
+        game.instance_variable_set(:@player_one, player_one)
+        game.instance_variable_set(:@whose_turn, player_one)
+        game.instance_variable_set(:@board, board)
+        return game
+      end
+
+      let(:board) { instance_double(ConnectFourBoard) }
+
+      context "when valid input" do
+        before do
+          valid_input = "0"
+          allow(Prompt).to receive(:get_input).and_return(valid_input)
+          allow(board).to receive(:column_full?).and_return false
+        end
+
+        it "stops loop" do
+          expect(game).not_to receive(:puts)
+          game.prompt_player_move
+        end
+      end
+
+      context "when invalid input" do
+        before do
+          invalid_input = "b"
+          valid_input = "0"
+          allow(Prompt).to receive(:get_input).and_return(invalid_input, valid_input)
+          allow(board).to receive(:column_full?).and_return false
+        end
+
+        it "loops once and displays proper message" do
+          text = "Input error: please enter a column number 0 through 6!".bold
+          expect(game).to receive(:puts).with(text).once
+          game.prompt_player_move
+        end
+      end
+
+      context "when column is full" do
+        before do
+          invalid_input = "1"
+          valid_input = "0"
+          allow(board).to receive(:column_full?).with(invalid_input.to_i).and_return true
+          allow(board).to receive(:column_full?).with(valid_input.to_i).and_return false
+          allow(Prompt).to receive(:get_input).and_return(invalid_input, valid_input)
+        end
+
+        it "loops once and displays proper message" do
+          text = "Input error: that column is full, choose another!".bold
+          expect(game).to receive(:puts).with(text).once
+          game.prompt_player_move
+        end
+      end
+    end
+  end
 end
